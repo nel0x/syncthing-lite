3e691b61c0c1cd0453c136ef7616c55c7ec7a25f
l-jonas <43265000+l-jonas@users.noreply.github.com>
2018-11-18 08:26:51 +0100

    Fix handling of paths with tilde (#91)
    * Refactor PathUtils

--- syncthing-core/src/main/kotlin/net/syncthing/java/core/utils/PathUtils.kt
+++ syncthing-core/src/main/kotlin/net/syncthing/java/core/utils/PathUtils.kt
@@ -1,5 +1,6 @@
 /* 
  * Copyright (C) 2016 Davide Imbriaco
+ * Copyright (C) 2018 Jonas Lochmann
  *
  * This Java file is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
@@ -13,20 +14,47 @@
  */
 package net.syncthing.java.core.utils
 
-import org.apache.commons.io.FilenameUtils
-
 object PathUtils {
-
-    val ROOT_PATH = ""
-    val PATH_SEPARATOR = "/"
-    val PARENT_PATH = ".."
-
-    private fun normalizePath(path: String): String {
-        return FilenameUtils.normalizeNoEndSeparator(path, true).replaceFirst(("^" + PATH_SEPARATOR).toRegex(), "")
-    }
+    const val ROOT_PATH = ""
+    const val PATH_SEPARATOR = "/"
+    const val PATH_SEPARATOR_WIN = "\\"
+    const val PARENT_PATH = ".."
+    const val CURRENT_PATH = "."
 
     fun isRoot(path: String): Boolean {
         return path.isEmpty()
+    }
+
+    private fun containsRelativeElements(path: String): Boolean {
+        val pathSegments = path.split(PATH_SEPARATOR)
+
+        return pathSegments.contains(PARENT_PATH) or pathSegments.contains(CURRENT_PATH)
+    }
+
+    private fun isTrimmed(value: String) = value.trim() == value
+    private fun containsWindowsPathSeparator(path: String) = path.contains(PATH_SEPARATOR_WIN)
+    private fun startsWithPathSeperator(path: String) = path.startsWith(PATH_SEPARATOR)
+    private fun isValidPath(path: String) = (!containsRelativeElements(path)) and
+            (!containsWindowsPathSeparator(path)) and
+            path.isNotEmpty() and
+            (!startsWithPathSeperator(path)) and
+            isTrimmed(path)
+
+    private fun containsPathSeparator(file: String) = file.contains(PATH_SEPARATOR) or file.contains(PATH_SEPARATOR_WIN)
+    private fun isFilenameValid(file: String) = file.isNotBlank() and
+            (!containsPathSeparator(file)) and
+            isTrimmed(file)
+
+    private fun assertPathValid(path: String) {
+        if (!isValidPath(path)) {
+            throw IllegalArgumentException("provided path is invalid")
+        }
+    }
+
+    private fun assertFilenameValid(filename: String) {
+        if (!isFilenameValid(filename)) {
+            throw IllegalArgumentException("provided filename is invalid")
+        }
     }
 
     fun isParent(path: String): Boolean {
@@ -34,15 +62,42 @@ object PathUtils {
     }
 
     fun getParentPath(path: String): String {
-        assert(!isRoot(path), {"cannot get parent of root path"})
-        return normalizePath(path + PATH_SEPARATOR + PARENT_PATH)
+        assertPathValid(path)
+
+        val pathWithoutSuffix = path.removeSuffix(PATH_SEPARATOR)
+        val previousSeparator = pathWithoutSuffix.lastIndexOf(PATH_SEPARATOR)
+
+        return if (previousSeparator == -1) {
+            ROOT_PATH
+        } else {
+            pathWithoutSuffix.substring(0, previousSeparator)
+        }
     }
 
     fun getFileName(path: String): String {
-        return FilenameUtils.getName(path)
+        if (path.isEmpty()) {
+            // this is required for IndexHandler.ROOT_FILE_INFO
+
+            return ""
+        }
+
+        assertPathValid(path)
+
+        val pathWithoutSuffix = path.removeSuffix(PATH_SEPARATOR)
+        val previousSeparator = pathWithoutSuffix.lastIndexOf(PATH_SEPARATOR)
+
+        return if (previousSeparator == -1) {
+            // the file is in the root directory
+            pathWithoutSuffix
+        } else {
+            pathWithoutSuffix.substring(previousSeparator + 1)
+        }
     }
 
     fun buildPath(dir: String, file: String): String {
-        return normalizePath(dir + PATH_SEPARATOR + file)
+        assertPathValid(dir)
+        assertFilenameValid(file)
+
+        return dir.removeSuffix(PATH_SEPARATOR) + file
     }
 }
