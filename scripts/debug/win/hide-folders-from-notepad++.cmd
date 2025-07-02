1fea3e4a51d42668430dd6b2d3fe88dcafd42386
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-02 17:27:09 +0200

    Create hide-folders-from-notepad++.cmd

---
+++ scripts/debug/win/hide-folders-from-notepad++.cmd
@@ -0,0 +1,22 @@
+@echo off
+cd ..\..\..
+::
+attrib +h .git
+attrib +h .github
+attrib +h .gradle
+attrib +h .idea
+attrib +h .kotlin
+attrib +h app\build
+attrib +h build
+attrib +h syncthing-bep\build
+attrib +h syncthing-client\build
+attrib +h syncthing-client-cli\build
+attrib +h syncthing-core\build
+attrib +h syncthing-discovery\build
+attrib +h syncthing-relay-client\build
+attrib +h syncthing-repository-android\build
+attrib +h syncthing-repository-default\build
+attrib +h syncthing-temp-repository-encryption\build
+timeout 3
+::
+goto :eof
