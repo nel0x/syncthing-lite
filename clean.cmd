702add64c38b974190ebebb744c8c46605c9bdfc
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-12 00:50:53 +0200

    Create clean.cmd

---
+++ clean.cmd
@@ -0,0 +1,15 @@
+@echo off
+::
+RD /S /Q ".gradle" 2>NUL:
+RD /S /Q ".kotlin" 2>NUL:
+RD /S /Q "app\build" 2>NUL:
+RD /S /Q "build" 2>NUL:
+RD /S /Q "syncthing-bep\build" 2>NUL:
+RD /S /Q "syncthing-client\build" 2>NUL:
+RD /S /Q "syncthing-core\build" 2>NUL:
+RD /S /Q "syncthing-discovery\build" 2>NUL:
+RD /S /Q "syncthing-relay-client\build" 2>NUL:
+RD /S /Q "syncthing-repository-android\build" 2>NUL:
+RD /S /Q "syncthing-temp-repository-encryption\build" 2>NUL:
+::
+goto :eof
