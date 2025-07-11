9871b81cddcbc52c569d5c68d8a7d921bffdd884
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-11 03:48:48 +0200

    update debug scripts

--- scripts/debug/win/reinstall.cmd
+++ scripts/debug/win/reinstall.cmd
@@ -1,11 +1,8 @@
 @echo off
 setlocal enabledelayedexpansion
 ::
-SET "ARTIFACT_PATH=X:\Shared"
-::
 adb uninstall net.syncthing.lite
 ::
-dir "%ARTIFACT_PATH%\app-debug.apk"
-adb install -r "%ARTIFACT_PATH%\app-debug.apk"
+call update.cmd
 ::
 goto :eof
---
+++ scripts/debug/win/update.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal enabledelayedexpansion
+::
+SET "ARTIFACT_PATH=X:\Shared"
+::
+dir "%ARTIFACT_PATH%\app-debug.apk"
+adb install -r "%ARTIFACT_PATH%\app-debug.apk"
+::
+goto :eof
