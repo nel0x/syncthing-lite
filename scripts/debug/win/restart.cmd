59bfa059df427f953f2cd91a8ba4c91fb2a91453
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-12 16:27:57 +0200

    Update restart.cmd

--- scripts/debug/win/restart.cmd
+++ scripts/debug/win/restart.cmd
@@ -4,6 +4,6 @@ setlocal enabledelayedexpansion
 SET "PACKAGE_NAME=com.github.catfriend1.syncthinglite.debug"
 ::
 adb shell am force-stop "%PACKAGE_NAME%"
-adb shell am start -n "%PACKAGE_NAME%/.activities.MainActivity"
+adb shell am start -n "%PACKAGE_NAME%/net.syncthing.lite.activities.MainActivity"
 ::
 goto :eof
