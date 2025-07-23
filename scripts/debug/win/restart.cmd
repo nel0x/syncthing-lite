264bd9b41aded247eca173edc9a04ba1c61ecdb6
Catfriend1 <16361913+Catfriend1@users.noreply.github.com>
2025-07-23 13:23:35 +0200

    restart: Clear file download cache

--- scripts/debug/win/restart.cmd
+++ scripts/debug/win/restart.cmd
@@ -4,6 +4,10 @@ setlocal enabledelayedexpansion
 SET "PACKAGE_NAME=com.github.catfriend1.syncthinglite.debug"
 ::
 call stop.cmd
+::
+:: adb shell rm -rf /data/data/%PACKAGE_NAME%/cache/
+adb shell rm -rf /storage/emulated/0/Android/data/%PACKAGE_NAME%/cache/
+::
 adb shell am start -n "%PACKAGE_NAME%/net.syncthing.lite.activities.MainActivity"
 ::
 goto :eof
